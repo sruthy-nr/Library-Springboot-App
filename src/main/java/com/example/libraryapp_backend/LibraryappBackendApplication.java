c1819b6940876210a9e1c0553c634fb12dc3e1f5
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-06 00:39:36 +0530

    first

---
+++ src/main/java/com/example/libraryapp_backend/LibraryappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryappBackendApplication.class, args);
+	}
+
+}
